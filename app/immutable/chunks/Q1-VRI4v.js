40f671b22588197949b312a5c20468d8d8d664ef
forewit <marcda495@gmail.com>
2025-07-30 11:00:59 -0400

    Updates

---
+++ app/immutable/chunks/Q1-VRI4v.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_1g306t1)==null?void 0:s.base)??"/fast-stack";var t;const e=((t=globalThis.__sveltekit_1g306t1)==null?void 0:t.assets)??a;export{e as a,a as b};
