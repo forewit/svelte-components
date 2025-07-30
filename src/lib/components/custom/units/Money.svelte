37b554e80a87eae1afb577d1aef77312e26900b7
forewit <marcda495@gmail.com>
2025-07-30 10:58:24 -0400

    na

--- src/lib/components/custom/units/Money.svelte
+++ src/lib/components/custom/units/Money.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import Input from "$lib/components/ui/input/input.svelte";
+  import { cn } from "$lib/utils";
   import type { HTMLInputAttributes } from "svelte/elements";
 
   type Props = Omit<
@@ -7,25 +8,35 @@
     "value" | "disabled" | "type" | "inputmode" | "placeholder" | "files" | "min"
   > & {
     value: number;
+    placeholder?: string;
     disabled?: boolean;
   };
 
-  let { value = $bindable(0), disabled = false, ...restProps }: Props = $props();
+  let {
+    value = $bindable(0),
+    disabled = false,
+    class: className,
+    placeholder = "0",
+    ...restProps
+  }: Props = $props();
 </script>
 
 {#if disabled}
   ${value.toFixed(2)}
 {:else}
   <span
-    class="relative before:content-['$'] before:absolute before:top-1/2 before:-translate-y-1/2 before:left-3"
+    class={cn("relative before:text-sm before:content-['$'] before:absolute before:top-1/2 before:-translate-y-1/2 before:left-3", className)}
   >
     <Input
       bind:value
       type="number"
       inputmode="decimal"
-      placeholder="0"
+      {placeholder}
+      class={cn(
+        "text-right pl-6",
+        className
+      )}
       {...restProps}
-      class="text-right pl-6"
     />
   </span>
 {/if}
